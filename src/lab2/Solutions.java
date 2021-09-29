adb7f448c3119a747573d77bb28988571f4158e7
merhawi <mwoldemichael>
2021-09-29 08:45:37 -0500

    lab2solutions1

--- src/lab2/Solutions.java
+++ src/lab2/Solutions.java
@@ -6,11 +6,11 @@ public class Solutions {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
 String fname,lname,middle;
-        System.out.println("input your first name");
+        System.out.println("input your First name");
         fname=input.next();
         System.out.println("input your middle name");
         middle=input.next();
-        System.out.println("input your last name");
+        System.out.println("input your Last name");
         lname=input.next();
         String fullname= fname + "  "+ middle.charAt(0)+"."+" "+lname;
         System.out.println(fullname);
